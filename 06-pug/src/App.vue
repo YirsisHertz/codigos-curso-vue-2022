4e9803adfc73bed08ba1b76e999bf06f8c3863cb
YirsisHertz <yirsishertz02@gmail.com>
2022-06-21 00:13:19 -0500

    pug

---
+++ 06-pug/src/App.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+
+lista(:elementos="juegos")
+
+</template>
+
+<script>
+import Lista from "./components/Lista.vue";
+
+export default {
+  data: () => ({
+    juegos: ["CSGO", "Minecraft", "PUBG"],
+  }),
+  components: { Lista },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+
+.elemento1 {
+  color: blue;
+}
+
+#elemento2 {
+  color: gold;
+}
+</style>
---
+++ 06-pug/src/components/Lista.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+ol
+  li(
+    v-for="elemento in elementos"
+    @click="select(elemento)"
+    ) {{elemento}}
+</template>
+
+<script>
+export default {
+  props: ["elementos"],
+  methods: {
+    select(value) {
+      alert("Seleccionaste: " + value);
+    },
+  },
+};
+</script>
+
+<style></style>
